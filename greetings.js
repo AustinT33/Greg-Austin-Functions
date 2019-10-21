d21df823990c88701b68f9054fc7b5dde4b0cdda
AustinT <austintumlinson@gmail.com>
2019-10-21 16:20:05 -0500

    year calc

--- greetings.js
+++ greetings.js
@@ -3,7 +3,8 @@
 function createGreeting (name,age) {
   let yourName = name;
   let yourAge = age;
-  return(`Hi, my name is ${yourName} and I\'m ${yourAge} years old`);
+  let yearOfBirth = (2019 - age)
+  return(`Hi, my name is ${yourName} and I\'m ${yourAge} years old, I was born in ${yearOfBirth}`);
 }
 
 const greeting1 = createGreeting();
