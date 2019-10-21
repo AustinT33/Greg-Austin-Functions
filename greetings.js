e607564db0a8bb270eb2126a0d1d975d3833ebd6
gdreid13 <gdreid13@gmail.com>
2019-10-21 14:39:13 -0700

    added more error checking

--- greetings.js
+++ greetings.js
@@ -9,6 +9,10 @@ function YearOfBirth(age) {
 function createGreeting (name,age) {
   let yourName = name;
   let yourAge = age;
+  if (yourName === undefined)
+    throw new Error('Arguments not valid');
+  if (yourAge === undefined)
+    throw new Error('Arguments not valid');
   let yob = YearOfBirth(yourAge);
   return(`Hi, my name is ${yourName} and I'm ${yourAge} years old, I was born in ${yob}`);
 }
