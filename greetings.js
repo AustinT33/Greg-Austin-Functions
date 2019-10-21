b1fa4a174210d6d7e9f561df81060d3cbc1e022c
gdreid13 <gdreid13@gmail.com>
2019-10-21 14:25:41 -0700

    added year of birth function

--- greetings.js
+++ greetings.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
 /* eslint-disable strict */
+function getYearOfBirth(age) {
+  return (2019 - age);
+}
+
 function createGreeting (name,age) {
   let yourName = name;
   let yourAge = age;
-  let yearOfBirth = (2019 - age)
-  return(`Hi, my name is ${yourName} and I\'m ${yourAge} years old, I was born in ${yearOfBirth}`);
+  let yob = getYearOfBirth(yourAge);
+  return(`Hi, my name is ${yourName} and I'm ${yourAge} years old, I was born in ${yob}`);
 }
 
 const greeting1 = createGreeting();
