f2b48c6830b46e6ba970f6ab787fc753944b2184
gdreid13 <gdreid13@gmail.com>
2019-10-21 14:37:04 -0700

    added error messaging

--- greetings.js
+++ greetings.js
@@ -1,16 +1,23 @@
 /* eslint-disable no-console */
 /* eslint-disable strict */
-function getYearOfBirth(age) {
+function YearOfBirth(age) {
+  if (2019-age < 0)
+    throw new Error('Age can not be negative');
   return (2019 - age);
 }
 
 function createGreeting (name,age) {
   let yourName = name;
   let yourAge = age;
-  let yob = getYearOfBirth(yourAge);
+  let yob = YearOfBirth(yourAge);
   return(`Hi, my name is ${yourName} and I'm ${yourAge} years old, I was born in ${yob}`);
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+try {
+  const greeting1 = createGreeting();
+}
+
+catch(error) {
+  console.error(error);
+}
 
