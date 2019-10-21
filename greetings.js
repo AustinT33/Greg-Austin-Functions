c59c1d6e2bcf63a126c2d82437fa7694240eb6e2
gdreid13 <gdreid13@gmail.com>
2019-10-21 14:13:43 -0700

    added variables

--- greetings.js
+++ greetings.js
@@ -1,6 +1,11 @@
-function createGreeting {
-    return('Hi, my name is Greg and I\'m 45 years old');
+/* eslint-disable no-console */
+/* eslint-disable strict */
+function createGreeting (name,age) {
+  let yourName = name;
+  let yourAge = age;
+  return(`Hi, my name is ${yourName} and I\'m ${yourAge} years old`);
 }
+
 const greeting1 = createGreeting();
 console.log(greeting1);
 
